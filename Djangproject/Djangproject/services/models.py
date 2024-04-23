7315a4e979d842b78917045b255106ab7d36dd9d
danish_ds <danishdeepasoman02@gmail.com>
2024-04-23 22:54:31 +0530

    Servicepage data migrate
    Servicepage this is done to migrate the data and also for setting the services page.Queries also change as it is needed.,

--- Djangproject/Djangproject/services/models.py
+++ Djangproject/Djangproject/services/models.py
@@ -1,3 +1,7 @@
 from django.contrib import admin 
+class Service(models.Model):
+    service_icon=models.CharField(max_length=50)
+    service_title=models.models.CharField(max_length=50)
+    service_des=models.TextField()                                    #python manage.py makemigrations  And should be able to see the services in the db as well 
 
-#Register your models here 
+#Register your models here   
