7579840b7207a07de89ae724cddaa661d765c8c0
danish_ds <danishdeepasoman02@gmail.com>
2024-05-23 15:55:51 +0530

    Upload a file - P1 (6.05)
    USe of upload the file using the django server - P1 6.05

--- Djangproject/Djangproject/urls.py
+++ Djangproject/Djangproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Djangproject import views
+from djangproject import views
+from django.conf import settings
+from djnago.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +34,7 @@ urlpatterns = [
     path('newsDetails/<slug>',views.newsDetails),
     path('saveEnquiry/',views.saveEnquiry,name="saveEnquiry"),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
